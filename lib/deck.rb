7c4d588c2ae14ba3ac36a3873e1bb816b47c870e
Jordan Skomal <jskomal@gmail.com>
2022-11-01 16:47:58 -0600

    Finish instances of deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,18 @@
 class Deck 
-  def initialize()
+  attr_reader :cards, :count
+  def initialize(cards)
+    @cards = cards
+    @count = cards.count
   end
+
+  def cards_in_category(input_category) 
+    result = []
+    @cards.each do |card|
+      if card.category == input_category
+        result << card
+      end
+    end
+    return result
+  end     
+
 end
