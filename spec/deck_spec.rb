70b2e61a1c330df17ca59f8e99a5373b15baf60b
Jordan Skomal <jskomal@gmail.com>
2022-11-01 16:39:16 -0600

    Add tests for Count and cards_in_category

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -18,7 +18,30 @@ RSpec.describe Deck do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    
+
     expect(deck.cards).to eq(cards)
   end
+
+  it 'keeps a count of the contained cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq(3)
+  end
+
+  it 'has a method to return an array of cards matching the input category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+    expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    expect(deck.cards_in_category('Pop Culture')).to eq([])
+  end
+
 end
