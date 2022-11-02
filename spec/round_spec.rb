db2c501d630a1e5c1a1062f7040ade5cba1f42e4
Jordan Skomal <jskomal@gmail.com>
2022-11-02 02:18:07 -0600

    Complete tests for round spec

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -116,6 +116,64 @@ RSpec.describe Round do
     expect(round.turns.count).to eq(2)
   end
 
+  it 'should be able to return previous feedback' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
 
+    expect(round.turns.last.feedback).to eq('No turns have been taken')
+
+    first_turn = round.take_turn('Juneau')
+
+    expect(round.turns.last.feedback).to eq('Correct!')
+
+    second_turn = round.take_turn('Feel Good Inc.')
+
+    expect(round.turns.last.feedback).to eq('Incorrect.')
+  end
+
+  it 'should count the number of correct answers in total, by category, by percent, and by percent in a category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.number_correct).to eq(0)
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
+    expect(round.percent_correct).to eq(0)
+    expect(round.percent_correct_by_category(:STEM)).to eq(0)
+
+    first_turn = round.take_turn('Juneau')
+
+    expect(round.number_correct).to eq(1)
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.percent_correct).to eq(100)
+    expect(round.percent_correct_by_category(:STEM)).to eq(0)
+    expect(round.percent_correct_by_category(:Geography)).to eq(100)
+
+    second_turn = round.take_turn('Feel Good Inc.')
+
+    expect(round.number_correct).to eq(1)
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.percent_correct).to eq(50)
+    expect(round.percent_correct_by_category(:STEM)).to eq(0)
+    expect(round.percent_correct_by_category(:Geography)).to eq(100)
+
+    third_turn = round.take_turn('North north west')
+
+    expect(round.number_correct).to eq(2)
+    expect(round.number_correct_by_category(:STEM)).to eq(1)
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.percent_correct).to eq(66.66)
+    expect(round.percent_correct_by_category(:STEM)).to eq(50)
+    expect(round.percent_correct_by_category(:Geography)).to eq(100)
+  end
 
 end
