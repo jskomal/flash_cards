6bda3abad68a79e877202cc84d9d65c31e86bc49
Jordan Skomal <jskomal@gmail.com>
2022-11-02 06:19:43 -0600

    Institute card generator

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,12 +2,9 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
-card_one = Card.new('What is 5 + 5?', '10', :STEM)
-card_two = Card.new("What is Rachel's favorite animal?", 'red panda', :Turing_Staff)
-card_three = Card.new("What is Mike's middle name", 'nobody knows', :Turing_Staff)
-card_four = Card.new('What cardboard cutout lives at Turing?', 'Justin Bieber', :Pop_Culture)
-cards = [card_one, card_two, card_three, card_four]
+cards = CardGenerator.new('cards.txt').create_cards
 deck = Deck.new(cards)
 round = Round.new(deck)
 
