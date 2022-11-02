e2ebef44d43d20df66597226de6616073c236f3e
Jordan Skomal <jskomal@gmail.com>
2022-11-02 03:33:42 -0600

    Complete tests 3-6

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,38 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
 
-  def initialize(deck) 
+  def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
+  end
+
+  def current_card
+    @deck.cards[@turns.count]
+  end
+
+  def take_turn(response)
+    current_turn = Turn.new(response, current_card)
+    @turns << current_turn
+    @number_correct += 1 if current_turn.correct?
+    current_turn
+  end
+
+  def number_correct_by_category(category)
+    result = 0
+    @turns.each do |turn|
+      result += 1 if turn.card.category == category && turn.correct?
+    end
+    result
+  end
+
+  def percent_correct
+    return 0 if @turns.count.zero?
+
+    @number_correct / @turns.count
+  end
+
+  def percent_correct_by_category(_category)
+    return 0 if @turns.count.zero?
   end
 end
